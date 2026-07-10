a77bbd1a0a86d4cd1d937cb0812c436547709fac
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-07-10 11:30:37 +0200

    Migration to V2.1 of base game

--- TreeSaplings-Redux-PreOiled_0.1.3/prototypes/recipe.lua
+++ TreeSaplings-Redux-PreOiled_0.1.3/prototypes/recipe.lua
@@ -6,7 +6,7 @@ data:extend({
 		name = "fertilizer",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/ammonium-nitrate.png",
 		icon_size = 32,
-		category = "chemistry",
+		categories = { "chemistry" },
 		ingredients =
 		{
 			{type="item", name="potassium-phosphate", amount=1},
@@ -22,7 +22,7 @@ data:extend({
 		name = "potassium-phosphate",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/potassium-phosphate.png",
 		icon_size = 32,
-		category = "chemistry",
+		categories = { "chemistry" },
 		ingredients =
 		{
 			{type="fluid", name="water", amount=50},
@@ -40,7 +40,7 @@ data:extend({
 		subgroup = "fluid-recipes",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/fluids/nitric-acid.png",
 		icon_size = 32,
-		category = "chemistry",
+		categories = { "chemistry" },
 		ingredients =
 		{
 			{type="fluid", name="water", amount=40},
@@ -56,7 +56,7 @@ data:extend({
 		name = "planter",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/planter.png",
 		icon_size = 32,
-		category = "crafting",
+		categories = { "crafting" },
 		ingredients = {{type="item", name="wood", amount=4}},
 		results = {{type="item", name="planter", amount=1}},
 		enabled = false
@@ -66,7 +66,7 @@ data:extend({
 		name = "sapling-grow-bag",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/sapling-grow-bag.png",
 		icon_size = 32,
-		category = "crafting",
+		categories = { "crafting" },
 		ingredients =
 		{
 			{type="item", name="wood", amount=1},
@@ -80,7 +80,7 @@ data:extend({
 		name = "grow-sapling",
 		icon = "__TreeSaplings-Redux-PreOiled__/graphics/icons/sapling.png",
 		icon_size = 32,
-		category = "growing",
+		categories = { "growing" },
 		ingredients = {{type="item", name="sapling-grow-bag", amount=1}},
 		results = {{type="item", name="wood", amount=4}},
 		energy_required = globalvalues.planter_grow_time,
